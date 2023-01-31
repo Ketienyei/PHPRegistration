e31fabbf655e5208e25698e571590292f840683d
Cynthia Ketienyei <cynthiachptoo@gmail.com>
2023-01-31 06:36:27 +0300

    working on delete and updata

--- templates/crud.php
+++ templates/crud.php
@@ -5,37 +5,65 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
+<?php
+if (isset($_POST ["delete_student"])){
+    $student_id = $_POST['delete_student'];
+
+    try{
+        $query = "DELETE FROM studentsdetails WHERE Id=:stud_id";
+        $statement =$databaseConnection->prepare($query);
+        $data = [
+            ':stud_id' => $student_id
+        ];
+        $statement->execute($data);
+        $query_execute = $statement->execute($data);
+        if ($query_execute){
+            echo'<script> alert("Are you sure you want to delete")</script>';
+            header('location:students.php');
+        
+        }else{
+            
+        }
+        }catch(PDOExeption $err){
+            echo $err->getMessage();
+        }
+    }
+    if (isset($_POST["update_student"])){
+        $student_id =htmlspecialchars(($_POST['student_id']);
+        $firstname =$_POST['firstname'];
+        $lastname =$_POST['lastname'];
+        $email =$_POST['email'];
+        $course =$_POST['course'];
+
+        try{
+            $query ="UPDATE studentsdetails SET firstname=:firstname, lastname=:lastaname, email=:email, course=:course";
+            $data = [
+                'firstname' => $firstname,
+                'lastname' => $lastname,
+                'email' => $email,
+                'course' => $course,
+                'stud_id' => $student_id
+            ];
+            $query_execute = $statement->execute($data);
+            if($query_execute){
+
+            };
+        
+        )
+    }
+
+    
+
+
+
+
+
+?>
 <body>
-   <div class="col-md-4 offset-md-4">
-    <h5>Edit Student's Details</h5>
-    <form action="crud.php" method="POST">
-        <input type="hidden" name="student_id" value="<?$results['id'];?>">
-
-        <div class="form-group">
-        <input type="test" name="firstname" value="<?$results['firstname'];?>" placeholder="Enter firstname" class="form-control">
-        </div>
-
-        <div class="form-group">
-        <input type="test" name="lastname" value="<?$results['lastname'];?>" placeholder="Enter lastname" class="form-control">
-        </div>
-
-        <div class="form-group">
-        <input type="test" name="email" value="<?$results['email'];?>" placeholder="Enter email" class="form-control">
-        </div>
-
-        <div class="form-group">
-        <input type="test" name="course" value="<?$results['course'];?>" placeholder="Enter course" class="form-control">
-        </div>
-
-        <button name="Delete_details" class="btn btn prrimary">Update <Details></Details></button>
-
-
-    </form>
-
-   </div>
-
+    
 </body>
 </html>
+
+
+
